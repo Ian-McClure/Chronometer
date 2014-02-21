2aa3ad207e14909f87297a8bdac9dc4d3c9e711c
Ian McClure <ianpmcclure@gmail.com>
2014-02-20 20:49:05 -0500

    Image factory

---
+++ Chronometer/Chronometer/ImageCache.h
@@ -0,0 +1,13 @@
+//
+//  ImageCache.h
+//  Chronometer
+//
+//  Created by Ian McClure on 2/20/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageCache : NSObject
+
+@end
---
+++ Chronometer/Chronometer/ImageCache.m
@@ -0,0 +1,13 @@
+//
+//  ImageCache.m
+//  Chronometer
+//
+//  Created by Ian McClure on 2/20/14.
+//
+//
+
+#import "ImageCache.h"
+
+@implementation ImageCache
+
+@end
