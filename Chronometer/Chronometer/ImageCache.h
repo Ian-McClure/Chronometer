b52626479b775c0061f8d15554fc2622ae5e5f1b
Ian McClure <ianpmcclure@gmail.com>
2014-02-21 20:23:36 -0500

    Added function to ImageCache object

--- Chronometer/Chronometer/ImageCache.h
+++ Chronometer/Chronometer/ImageCache.h
@@ -2,12 +2,18 @@
 //  ImageCache.h
 //  Chronometer
 //
-//  Created by Ian McClure on 2/20/14.
+//  Created by Matthew Breton & Ian McClure on 2/20/14.
 //
 //
 
 #import <Foundation/Foundation.h>
 
 @interface ImageCache : NSObject
+
++ (id)sharedImageCache;
+//- (id)init;
+
+- (UIImage *)imageNamed:(NSString *)string;
+- (UIImage *)imageForTumbler:(int)digit half:(BOOL)top place:(int)place;
 
 @end
--- Chronometer/Chronometer/ImageCache.m
+++ Chronometer/Chronometer/ImageCache.m
@@ -2,12 +2,60 @@
 //  ImageCache.m
 //  Chronometer
 //
-//  Created by Ian McClure on 2/20/14.
+//  Created by Matthew Breton & Ian McClure on 2/20/14.
 //
 //
 
 #import "ImageCache.h"
 
 @implementation ImageCache
+
+//Dispatch tokens are markers used by Grand Central Dispatch to ensure that this object is instantiated only once.
+//Look up Singletons for more info.
+
++ (id)sharedImageCache {
+    static ImageCache *sharedImageCache = nil;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        sharedImageCache = [[self alloc] init];
+    });
+    
+    return sharedImageCache;
+}
+
+- (UIImage *)imageNamed:(NSString *)string {
+    
+    NSString *tag = [string substringFromIndex:(string.length - 4)];
+    string = ([tag isEqualToString:@".png"]) ? string : [string stringByAppendingString:@".png"];
+
+    return [UIImage imageNamed:string];
+}
+
+- (UIImage *)imageForTumbler:(int)digit half:(BOOL)top place:(int)place {
+    
+    NSString *string = [NSString stringWithFormat:@"tumbler%d", digit];
+
+    if (place == 2 || place == 4) {
+        string = [string stringByAppendingString:@"rightcolon"];
+    } else if (place == 1 || place == 3) {
+        string = [string stringByAppendingString:@"leftCcolon"];
+    } else {
+        string = [string stringByAppendingString:@"nocolon"];
+    }
+    string = (top) ? [string stringByAppendingString:@"top"] : [string stringByAppendingString:@"bottom"];
+    
+    UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
+    if (orientation == UIInterfaceOrientationLandscapeLeft || orientation == UIInterfaceOrientationLandscapeRight) {
+        if ([UIScreen mainScreen].bounds.size.height) {
+            string = [string stringByAppendingString:@"568hlandscape"];
+        } else {
+            string = [string stringByAppendingString:@"landscape"];
+        }
+    }
+    string = [string stringByAppendingString:@".png"];
+    
+    return [UIImage imageNamed:string];
+}
 
 @end
