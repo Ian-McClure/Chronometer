9076d92ed97cb54a7c0d236ff66524f920b268bb
Ian McClure <ianpmcclure@gmail.com>
2014-05-19 08:56:44 -0400

    Updated the look on the interval view.

---
+++ Chronometer/Chronometer/IntervalTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  IntervalTableViewCell.h
+//  Chronometer
+//
+//  Created by Ian McClure on 5/19/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IntervalTableViewCell : UITableViewCell
+
+@end
---
+++ Chronometer/Chronometer/IntervalTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  IntervalTableViewCell.m
+//  Chronometer
+//
+//  Created by Ian McClure on 5/19/14.
+//
+//
+
+#import "IntervalTableViewCell.h"
+
+@implementation IntervalTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
